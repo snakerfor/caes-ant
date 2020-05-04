bd61b4a49ccb30d66f906ce463a4a37c4d39fb4d
snakerfor <snaker_dm@163.com>
2020-05-05 00:27:19 +0800

    修复文章发布菜单tinymce 组件无key问题

--- src/views/competition/EssayEditor.vue
+++ src/views/competition/EssayEditor.vue
@@ -2,7 +2,7 @@
   <a-card
     :body-style="{padding: '24px 32px'}"
     :bordered="false">
-    <tinymce v-model="value"></tinymce>
+    <tinymce :key="tinymceFlag" v-model="value"></tinymce>
     {{ value }}
     <div v-html="value"></div>
     <div v-html="testLink"></div>
@@ -20,6 +20,7 @@ export default {
     return {
       description: '文章编辑与发布管理',
       value: '',
+      tinymceFlag: 1,
       downloadFile: downloadFile,
       testLink: '<p>测试附件下载：<a href="#" onclick="downloadFile(\'e12ac315195e4f0d92ac783c8e49120e\')">LinZhaoguan-paste-upload-master.zip</a></p>'
     }
@@ -29,6 +30,9 @@ export default {
     window['downloadFile'] = (fileId) => {
       downloadFile(fileId)
     }
+  },
+  activated () {
+    this.tinymceFlag++
   }
 }
 </script>
